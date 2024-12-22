546ef835d4f2c1d82ffc45af5d6eb3769de629ae
bakulev_hs <bakulev_hs@teleformis.ru>
2024-12-22 11:22:47 +0300

    Добавление описания проекта.

--- src/Model/Include/Blocks/Iblock.h
+++ src/Model/Include/Blocks/Iblock.h
@@ -7,58 +7,58 @@
 namespace Tetris::Model::Blocks
 {
 
-	/// @brief класс I блок
-	class Iblock final : public AbstractBlock
-	{
-	public:
-		/// @brief Конструктор
-		/// @param color цвет блока
-		Iblock(TypeColor color) : AbstractBlock(color)
-		{
-			_stateToPositions = {
-				{State::Up,
-					{
-						Position(-1, 0), // *  *  *  *  *
-						Position( 0, 0), // *  *  *  *  *
-						Position( 1, 0), // *  о  0  о  о
-						Position( 2, 0)	 // *  *  *  *  *
-					}				     // *  *  *  *  *
-				},
-				{State::Right,
-					{
-						Position(0, 1), // *  *  *  *  *
-						Position(0, 0), // *  *  о  *  *
-						Position(0,-1), // *  *  0  *  *
-						Position(0,-2)  // *  *  о  *  *
-					}				    // *  *  о  *  *
-				},					   
-				{State::Down,
-					{
-						Position( 1, 0), // *  *  *  *  *
-						Position( 0, 0), // *  *  *  *  *
-						Position(-1, 0), // о  о  0  о  *
-						Position(-2, 0)  // *  *  *  *  *
-					}				     // *  *  *  *  *
-				},					     
-				{State::Left,
-					{
-						Position( 0,-1), // *  *  о  *  *
-						Position( 0, 0), // *  *  о  *  *
-						Position( 0, 1), // *  *  0  *  *
-						Position( 0, 2)  // *  *  о  *  *
-					}				     // *  *  *  *  *
-				} 
-			};
+    /// @brief класс I блок
+    class Iblock final : public AbstractBlock
+    {
+    public:
+        /// @brief Конструктор
+        /// @param color цвет блока
+        Iblock(TypeColor color) : AbstractBlock(color)
+        {
+            _stateToPositions = {
+                {State::Up,
+                    {
+                        Position(-1, 0), // *  *  *  *  *
+                        Position( 0, 0), // *  *  *  *  *
+                        Position( 1, 0), // *  о  0  о  о
+                        Position( 2, 0)     // *  *  *  *  *
+                    }                     // *  *  *  *  *
+                },
+                {State::Right,
+                    {
+                        Position(0, 1), // *  *  *  *  *
+                        Position(0, 0), // *  *  о  *  *
+                        Position(0,-1), // *  *  0  *  *
+                        Position(0,-2)  // *  *  о  *  *
+                    }                    // *  *  о  *  *
+                },                       
+                {State::Down,
+                    {
+                        Position( 1, 0), // *  *  *  *  *
+                        Position( 0, 0), // *  *  *  *  *
+                        Position(-1, 0), // о  о  0  о  *
+                        Position(-2, 0)  // *  *  *  *  *
+                    }                     // *  *  *  *  *
+                },                         
+                {State::Left,
+                    {
+                        Position( 0,-1), // *  *  о  *  *
+                        Position( 0, 0), // *  *  о  *  *
+                        Position( 0, 1), // *  *  0  *  *
+                        Position( 0, 2)  // *  *  о  *  *
+                    }                     // *  *  *  *  *
+                } 
+            };
 
-			_stateToOffset = IOffset;
-		};
+            _stateToOffset = IOffset;
+        };
 
-		/// @brief Получение типа блока
-		/// @return Идентификатор формы блока
-		TypeBlock GetType() const
-		{
-			return TypeBlock::Iblock;
-		};
-	};
+        /// @brief Получение типа блока
+        /// @return Идентификатор формы блока
+        TypeBlock GetType() const
+        {
+            return TypeBlock::Iblock;
+        };
+    };
 
 } // namespace Tetris::Model::Blocks
